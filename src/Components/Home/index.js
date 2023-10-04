27aa0cfc738f64c95f2015b0a8ecd1309845aab6
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-10-04 20:23:14 +0100

    home page update

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,4 +1,5 @@
 
+import { Link } from 'react-router-dom';
 import Nav from '../Nav';
 
 import './index.css';
@@ -6,15 +7,36 @@ import './index.css';
 function Home() {
   return (
     <div className="App">
-    <Nav/>
+    {/* <Nav/> */}
       <header className="App-header">
-       
+      <div className='boxItem'>
+<p>
+  Accounting
+  <br></br>
+  You Can
+  <br></br>
+  Count On
+  <br></br>
+  Get started
+  <br></br>
+  with a free
+  <br></br>
+  consultation
+  <br></br>
+  !
+</p>
+<div>
+<li><button><Link to="/contact">Contact</Link></button></li>
+</div>
+
+</div>
         <p>
           Website under construction - please check back soon!
         </p>
       More info: <a href="mailto:info@gulamabbasaccounts.co.uk">info@gulamabbasaccounts.co.uk
         </a>
       </header>
+     
     </div>
   );
 }
